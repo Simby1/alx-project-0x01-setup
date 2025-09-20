fae3c502b19c4529f4ed0bb7dfc7f14cf5006b47
Simby1 <kimsimby@gmail.com>
2025-09-20 12:21:23 +0100

    setup project 2

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostCard: React.FC = () => {
+  return (
+    <div>
+      Post Card
+    </div>
+  );
+};
+
+export default PostCard;
---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Posts: React.FC = () => {
+  return (
+    <div>
+      Posts Page
+    </div>
+  );
+};
+
+export default Posts;
