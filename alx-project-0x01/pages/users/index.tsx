aa4c10e88545925c036f38eca5c3cbdaaa920a6a
Simby1 <kimsimby@gmail.com>
2025-09-20 13:30:30 +0100

    implement usermodal

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,15 +1,24 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
-import { UserProps } from "@/interfaces";
+import UserModal from "@/components/common/UserModal";
+import { UserData, UserProps } from "@/interfaces";
+import { useState } from "react";
 
 const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [user, setUser] = useState<UserData | null>(null);
+
+  const handleAddUser = (newUser: UserData) => {
+    setUser({ ...newUser });
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-semibold">User Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+          <button onClick={() => setModalOpen(true)} className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
           {users.map((user) => (
@@ -17,6 +26,9 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
           ))}
         </div>
       </main>
+      {isModalOpen && (
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddUser} />
+      )}
     </div>
   );
 };
